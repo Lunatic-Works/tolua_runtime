6dc467101704477c8ac22490d9dfcac04288b47e
woctordho <woctordho@outlook.com>
2024-05-10 15:18:25 +0200

    Update Linux build

---
+++ build_linux.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# 64 bit version
+# build for Ubuntu 24.04
+
+cd luajit-2.1 || exit
+# make clean
+make BUILDMODE=static CC="gcc -fPIC -m64 -O3" XCFLAGS=-DLUAJIT_ENABLE_GC64
+echo -e "\n[MAINTENANCE] build libluajit.a done\n"
+cd ..
+
+mkdir -p Plugins/Linux
+gcc -m64 -O3 -Wall -Wextra -std=gnu99 -shared \
+ src/int64.c \
+ src/struct.c \
+ src/tolua.c \
+ src/uint64.c \
+ `# cjson/fpconv.c` \
+ `# cjson/lua_cjson.c` \
+ `# cjson/strbuf.c` \
+ `# lpeg/lpeg.c` \
+ `# luasocket/auxiliar.c` \
+ `# luasocket/buffer.c` \
+ `# luasocket/except.c` \
+ `# luasocket/inet.c` \
+ `# luasocket/io.c` \
+ `# luasocket/luasocket.c` \
+ `# luasocket/mime.c` \
+ `# luasocket/options.c` \
+ `# luasocket/select.c` \
+ `# luasocket/tcp.c` \
+ `# luasocket/timeout.c` \
+ `# luasocket/udp.c` \
+ `# luasocket/usocket.c` \
+ `# protobuf/pb.c` \
+ -fPIC \
+ -Isrc \
+ -Iluajit-2.1/src \
+ `# -Iluasocket` \
+ -Wl,--whole-archive luajit-2.1/src/libluajit.a \
+ -Wl,--no-whole-archive \
+ -static-libgcc -static-libstdc++ \
+ -o Plugins/Linux/libtolua.so
+if [ "$?" = "0" ]; then
+    echo -e "\n[MAINTENANCE] build libtolua.so done"
+else
+    echo -e "\n[MAINTENANCE] build libtolua.so failed"
+fi
