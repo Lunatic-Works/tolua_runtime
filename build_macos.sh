6e531fbc90a67273318f2f482dd7aaf3f6e052e7
woctordho <woctordho@outlook.com>
2024-05-10 18:14:33 +0200

    Update README.md

--- build_macos.sh
+++ build_macos.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
+mkdir -p Plugins/macOS
 cd macnojit/
 xcodebuild clean
 xcodebuild -configuration=Release ONLY_ACTIVE_ARCH=NO
-cp -r build/Release/tolua.bundle ../Plugins/
+cp -r build/Release/tolua.bundle ../Plugins/macOS/
